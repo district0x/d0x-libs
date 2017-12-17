d802ba1885c1b2d92f549e484044d5e1070af860
madvas <matus.lestan@gmail.com>
2017-12-17 11:03:08 +0100

    first commit

---
+++ server/district-server-web3-watcher/project.clj
@@ -0,0 +1,10 @@
+(defproject district0x/district-server-web3-watcher "1.0.0"
+  :description "district0x server component for setting up web3 watcher"
+  :url "https://github.com/district0x/district-server-web3-watcher"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[cljs-web3 "0.19.0-0-8"]
+                 [district0x/district-server-config "1.0.0"]
+                 [district0x/district-server-web3 "1.0.0"]
+                 [mount "0.1.11"]
+                 [org.clojure/clojurescript "1.9.946"]])
