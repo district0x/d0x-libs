a2047f6bad68758e93c7521f15948c3e95426ec6
madvas <matus.lestan@gmail.com>
2017-12-18 20:15:42 +0100

    Added tests

--- server/district-server-web3/project.clj
+++ server/district-server-web3/project.clj
@@ -3,10 +3,32 @@
   :url "https://github.com/district0x/district-server-web3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljs-web3 "0.19.0-0-8"]
-                 [district0x/district-server-config "1.0.0"]
+  :dependencies [[district0x/district-server-config "1.0.0"]
                  [mount "0.1.11"]
                  [org.clojure/clojurescript "1.9.946"]]
 
-  :npm {:dependencies [[ganache-core "2.0.2"]
-                       [web3 "0.19.0"]]})
+  :npm {:dependencies [[web3 "0.19.0"]]
+        :devDependencies [[ws "2.0.1"]]}
+
+  :figwheel {:server-port 4673}
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [com.cemerick/piggieback "0.2.2"]
+                                  [figwheel-sidecar "0.5.14"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
+                                  [cljs-web3 "0.19.0-0-8"]]
+                   :plugins [[lein-cljsbuild "1.1.7"]
+                             [lein-figwheel "0.5.14"]
+                             [lein-npm "0.6.2"]
+                             [lein-doo "0.1.7"]]
+                   :source-paths ["dev"]}}
+
+  :cljsbuild {:builds [{:id "tests"
+                        :source-paths ["src" "test"]
+                        :figwheel {:on-jsload "tests.runner/-main"}
+                        :compiler {:main "tests.runner"
+                                   :output-to "tests-compiled/run-tests.js"
+                                   :output-dir "tests-compiled"
+                                   :target :nodejs
+                                   :optimizations :none
+                                   :source-map true}}]})
