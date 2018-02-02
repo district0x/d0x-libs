577e00a24fa4d565610695cc11e62ac9c1f3348a
madvas <matus.lestan@gmail.com>
2018-02-02 10:28:03 -0800

    first commit

---
+++ browser/district-ui-component-active-account-balance/project.clj
@@ -0,0 +1,10 @@
+(defproject district0x/district-ui-component-active-account-balance "1.0.0"
+  :description "district UI component for displaying ETH or token balance of an active web3 account"
+  :url "https://github.com/district0x/district-ui-component-active-account-balance"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[district0x/district-format "1.0.0"]
+                 [district0x/district-ui-web3-account-balances "1.0.1"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [re-frame "0.10.2"]])
