13d8205ba689f7691ed7e33828f3ce2fb026018f
madvas <matus.lestan@gmail.com>
2019-04-27 14:50:39 +0700

    Add function register-after-past-events-dispatched-callback!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/district-server-web3-events "1.0.1-SNAPSHOT"
+(defproject district0x/district-server-web3-events "1.0.2-SNAPSHOT"
   :description "district0x server module for handling web3 events"
   :url "https://github.com/district0x/district-server-web3-events"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
