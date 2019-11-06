24854892eb91bd02ec2817442f6e98d54d2610e2
filip <fbielejec@gmail.com>
2019-11-06 09:49:28 +0100

    web3++

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject district0x/district-server-web3 "1.1.2-SNAPSHOT"
+(defproject district0x/district-server-web3 "1.1.3-SNAPSHOT"
   :description "district0x server module for setting up web3"
   :url "https://github.com/district0x/district-server-web3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljs-web3-next "0.0.14"]
+  :dependencies [[cljs-web3-next "0.0.15"]
                  [com.taoensso/timbre "4.10.0"]
                  [district0x/district-server-config "1.0.1"]
                  [mount "0.1.16"]
@@ -35,8 +35,7 @@
 
   :cljsbuild {:builds [{:id "nodejs-tests"
                         :source-paths ["src" "test"]
-                        :compiler {
-                                   :output-to "tests-compiled/run-tests.js"
+                        :compiler {:output-to "tests-compiled/run-tests.js"
                                    :output-dir "tests-compiled"
                                    :main "tests.runner"
                                    :target :nodejs
