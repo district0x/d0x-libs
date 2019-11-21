401b4a3f57c65057ed338ef5a16ac6617dd81627
filip <fbielejec@gmail.com>
2019-11-21 19:27:40 +0100

    this version was released

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/district-server-web3-events "1.1.0-SNAPSHOT"
+(defproject district0x/district-server-web3-events "1.1.5-SNAPSHOT"
   :description "district0x server module for handling web3 events"
   :url "https://github.com/district0x/district-server-web3-events"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
