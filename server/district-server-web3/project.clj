1f4ccffe5c40f7771b4fd685bbe03d511ed9a2d0
Filip Bielejec <fbielejec@gmail.com>
2020-05-08 10:15:16 +0200

    implementing exponential backoff (#8)

--- server/district-server-web3/project.clj
+++ server/district-server-web3/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cljs-web3-next "0.1.2"]
                  [com.taoensso/timbre "4.10.0"]
+                 [district0x/async-helpers "0.1.3"]
                  [district0x/district-server-config "1.0.1"]
                  [mount "0.1.16"]
                  [org.clojure/clojurescript "1.10.520"]]
@@ -12,8 +13,7 @@
   :npm {:dependencies [[web3 "1.2.0"]]
         :devDependencies [[ws "2.0.1"]]}
 
-  :profiles {:dev {:dependencies [[district0x/async-helpers "0.1.3"]
-                                  [org.clojure/clojure "1.10.1"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [org.clojure/core.async "0.4.500"]]
                    :plugins [[lein-cljsbuild "1.1.7"]
                              [lein-npm "0.6.2"]
