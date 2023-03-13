26ccccff339fc83e0a99b83daa43b36d375a8e30
ginesdt <ginesdt@gmail.com>
2023-03-13 11:01:23 +0100

    Allow SQL format options

--- server/district-server-db/test/tests/main_test.cljs
+++ server/district-server-db/test/tests/main_test.cljs
@@ -18,30 +18,70 @@
   ; (is (= (str @db) "[object Database]"))
 
   (is (map? (db/run! {:create-table [:my-doggos]
-                      :with-columns [[[:doggo/years :unsigned :integer]
-                                      [:doggo/description :varchar]]]})))
+                      :with-columns [[[:years :unsigned :integer]
+                                      [:description :varchar]]]})))
 
   (is (= (db/run! {:insert-into :my-doggos
-                   :columns [:doggo/years :doggo/description]
+                   :columns [:years :description]
                    :values [[1 "Good boy"]]})
          {:changes 1 :lastInsertRowid 1}))
 
-  (is (= (db/get {:select [:doggo/description]
+  (is (= (db/get {:select [:description]
                   :from [:my-doggos]
-                  :where [:= :doggo/years 1]})
+                  :where [:= :years 1]})
          {:description "Good boy"}))
 
-  (is (= (db/all {:select [:doggo/description]
+  (is (= (db/all {:select [:description]
                   :from [:my-doggos]
-                  :where [:= :doggo/years 1]})
+                  :where [:= :years 1]})
          [{:description "Good boy"}]))
 
   (is (= (db/run! {:insert-into :my-doggos
-                   :columns [:doggo/years :doggo/description]
+                   :columns [:years :description]
                    :values [[2 "Bad boy"]]})
          {:changes 1 :lastInsertRowid 2}))
 
-  (is (= (db/total-count {:select [:doggo/description]
+  (is (= (db/total-count {:select [:description]
                           :from [:my-doggos]
                           :limit 1})
          2)))
+
+(deftest test-db-w-ns
+
+  (let [opts {:format-opts {:allow-namespaced-names? true}}]
+
+    (is (map? (db/run! {:create-table [:my-doggos]
+                        :with-columns [[[:doggo/years :unsigned :integer]
+                                        [:doggo/description :varchar]
+                                        [:other/description :varchar]]]}
+                       opts)))
+
+    (is (= (db/run! {:insert-into :my-doggos
+                     :columns [:doggo/years :doggo/description]
+                     :values [[1 "Good boy"]]}
+                    opts)
+           {:changes 1 :lastInsertRowid 1}))
+
+    (is (= (db/get {:select [:doggo/description]
+                    :from [:my-doggos]
+                    :where [:= :doggo/years 1]}
+                   opts)
+           {:doggo/description "Good boy"}))
+
+    (is (= (db/all {:select [:doggo/description]
+                    :from [:my-doggos]
+                    :where [:= :doggo/years 1]}
+                   opts)
+           [{:doggo/description "Good boy"}]))
+
+    (is (= (db/run! {:insert-into :my-doggos
+                     :columns [:doggo/years :doggo/description]
+                     :values [[2 "Bad boy"]]}
+                    opts)
+           {:changes 1 :lastInsertRowid 2}))
+
+    (is (= (db/total-count {:select [:doggo/description]
+                            :from [:my-doggos]
+                            :limit 1})
+           2))
+    ))
