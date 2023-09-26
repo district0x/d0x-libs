7525efb6c054b040bef2031b856b630df6a43a81
ginesdt <81330305+ginesdt@users.noreply.github.com>
2023-09-26 11:20:58 +0200

    Sendgrid fix and improvements (#65)
    * fix sendgrid API request
    
    * add sendgrid dynamic template support

--- shared/district-sendgrid/src/district/sendgrid.cljs
+++ shared/district-sendgrid/src/district/sendgrid.cljs
@@ -15,7 +15,7 @@
 ;   https://docs.sendgrid.com/api-reference/mail-send/mail-send
 (defn send-email
   [{:keys [:from :to :subject :content :substitutions :on-success :on-error :template-id :api-key :body :headers
-           :print-mode?]}]
+           :dynamic-template-data :print-mode?]}]
   (if (and (not api-key)
            (not print-mode?))
     (throw (js/Error. "Missing api-key to send email to sendgrid"))
@@ -26,19 +26,21 @@
         (println "To:" to)
         (println "Subject:" subject)
         (println "Content:" content)
-        (println "Substitutions:" substitutions))
+        (println "Substitutions:" substitutions)
+        (println "Dynamic-template-data:" dynamic-template-data))
       (let [headers (merge {"Authorization" (str "Bearer " api-key)
                             "Content-Type" "application/json"} headers)
             body (merge {:from {:email from}
-                         :personalizations [{:to [{:email to}]
-                                             ;; Substitutions are in format e.g ":header", so (str :header) works well
-                                             :substitutions (into {} (map (fn [[k v]] [(str k) v]) substitutions))}]
+                         :personalizations [(cond-> {:to [{:email to}]}
+                                                    dynamic-template-data (assoc :dynamic_template_data dynamic-template-data)
+                                                    ;; Substitutions are in format e.g ":header", so (str :header) works well
+                                                    substitutions (assoc :substitutions (into {} (map (fn [[k v]] [(str k) v]) substitutions))))]
                          :subject subject
                          :content [{:type "text/html"
                                     :value content}]
                          :template_id template-id}
                         body)]
-        (async/take! (http/post sendgrid-public-api {:headers headers} {:json-params body})
+        (async/take! (http/post sendgrid-public-api {:headers headers :json-params body})
                      (fn [res] (if (= true (:success res))
                                  (on-success res)
                                  (on-error res))))
