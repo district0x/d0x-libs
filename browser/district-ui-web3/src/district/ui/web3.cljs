509837de031d96cddc1522b6b33246a6afceae09
ginesdt <ginesdt@gmail.com>
2023-11-30 14:05:41 +0100

    Allow disabling connecting wallet on init

--- browser/district-ui-web3/src/district/ui/web3.cljs
+++ browser/district-ui-web3/src/district/ui/web3.cljs
@@ -15,7 +15,7 @@
 (s/def ::url string?)
 (s/def ::wait-for-inject-ms number?)
 (s/def ::opts (s/keys :req-un [::url]
-                      :opt-un [::wait-for-inject-ms ::authorize-on-init?]))
+                      :opt-un [::wait-for-inject-ms ::connect-on-init? ::authorize-on-init?]))
 
 (defn start [opts]
   (s/assert ::opts opts)
